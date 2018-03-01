8749793cdd9c63289299ca761db0d5cd96b23e92
Dennis <dennisaguilar1989@gmail.com>
2018-03-01 06:14:22 -0500

    Added a save button to annual compound interest. Organized objects  prior to class loading.

--- app/src/main/java/tech/extropy/dennis/compoundinterestcalculator/AnnualCompoundInterest.java
+++ app/src/main/java/tech/extropy/dennis/compoundinterestcalculator/AnnualCompoundInterest.java
@@ -1,11 +1,14 @@
 package tech.extropy.dennis.compoundinterestcalculator;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.KeyEvent;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.WindowManager;
 import android.view.inputmethod.EditorInfo;
@@ -18,18 +21,12 @@ import java.text.DecimalFormat;
 
 import tech.extropy.dennis.compoundinterestcalculator.Controller.Stack;
 import tech.extropy.dennis.compoundinterestcalculator.Math.FinanceMath;
+import tech.extropy.dennis.compoundinterestcalculator.Model.DatabaseHelper;
 
 public class AnnualCompoundInterest extends AppCompatActivity {
-    TextView mYearGrow;
-    TextView mInterestRate;
-    TextView mCurrentPrinciple;
-    TextView mNumberOfTimesCompounded;
-    EditText mYearGrowInput;
-    EditText mInterestRateInput;
-    EditText mCurrentPrincipleInput;
-    EditText mNumberOfTimesCompoundedInput;
+    TextView mYearGrow, mInterestRate, mCurrentPrinciple, mNumberOfTimesCompounded, mTotal;
+    EditText mYearGrowInput, mInterestRateInput, mCurrentPrincipleInput, mNumberOfTimesCompoundedInput;
     Button mCalculate;
-    TextView mTotal;
     FinanceMath finance;
     DecimalFormat df2;
     String strInput;
@@ -40,6 +37,8 @@ public class AnnualCompoundInterest extends AppCompatActivity {
     double total;
     boolean checkValidation;
     Intent intent;
+    Button mSave;
+    DatabaseHelper mDatabaseHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,12 +55,15 @@ public class AnnualCompoundInterest extends AppCompatActivity {
         mTotal = (TextView) findViewById(R.id.total);
         df2 = new DecimalFormat(".##");
         mCalculate = (Button) findViewById(R.id.calculate);
+        mSave = (Button) findViewById(R.id.save);
         finance = new FinanceMath();
         checkValidation = false;
         intent = getIntent();
+        mDatabaseHelper = new DatabaseHelper(this);
 
         mTotal.setText("Total: $0.00");
         Context context = getApplicationContext();
+        final Context context1 = this;
         CharSequence text = "Hello toast!";
         int duration = Toast.LENGTH_SHORT;
 
@@ -128,6 +130,86 @@ public class AnnualCompoundInterest extends AppCompatActivity {
                 }
 
             }});
+// add button listener
+        mSave.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                // get prompts.xml view
+                LayoutInflater li = LayoutInflater.from(context1);
+                View promptsView = li.inflate(R.layout.custom, null);
+
+                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(context1);
+
+                // set prompts.xml to alertdialog builder
+                alertDialogBuilder.setView(promptsView);
+
+                final EditText userInput = (EditText) promptsView
+                        .findViewById(R.id.editTextDialogUserInput);
+
+                // set dialog message
+                alertDialogBuilder
+                        .setCancelable(true)
+                        .setPositiveButton("OK",
+                                new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog,int id) {
+
+                                        Stack newStack = new Stack();
+
+                                        // get user input and set it to result
+                                        // edit text
+                                        //intent = getIntent();
+                                        int formulaType = intent.getIntExtra("type", 9999);
+
+                                        //Log.d("formulaType234987 :",""+formulaType);
+
+                                        //newStack.push(4);
+                                        //Copy stack from previous activity to the next.
+                                        int[] stackArr = intent.getIntArrayExtra("intArr");
+                                        newStack.setStackArr(stackArr);
+                                        int top = intent.getIntExtra("top",9999);
+                                        newStack.setTop(top);
+                                        newStack.push(4);
+
+                                        newStack.printAll();
+
+                                        String newEntry = userInput.getText().toString();
+                                        if (userInput.length() != 0) {
+                                            AddData(newEntry);
+                                            userInput.setText("");
+
+                                            //View list of data
+                                            Intent intent = new Intent(AnnualCompoundInterest.this, ListDataActivity.class);
+                                            intent.putExtra("type", 5);
+                                            intent.putExtra("top", newStack.getTop());
+                                            intent.putExtra("intArr", stackArr);
+
+                                            startActivity(intent);
+                                            finish();
+                                        } else {
+                                            toastMessage("You must put something in the text field!");
+                                        }
+
+                                        //mResult.setText(test);
+                                    }
+                                })
+                        .setNegativeButton("Cancel",
+                                new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog,int id) {
+                                        dialog.cancel();
+                                    }
+                                });
+
+                // create alert dialog
+                AlertDialog alertDialog = alertDialogBuilder.create();
+
+                // show it
+                alertDialog.show();
+
+            }
+        });
+
     }
 
     @Override
@@ -147,6 +229,24 @@ public class AnnualCompoundInterest extends AppCompatActivity {
         nextIntent.putExtra("top",newStack.getTop());
         startActivity(nextIntent);
         finish();
+    }
+
+    public void AddData(String newEntry) {
+        boolean insertData = mDatabaseHelper.addData(newEntry);
+
+        if (insertData) {
+            toastMessage("Data Successfully Inserted!");
+        } else {
+            toastMessage("Something went wrong");
+        }
+    }
+
+    /**
+     * customizable toast
+     * @param message
+     */
+    private void toastMessage(String message){
+        Toast.makeText(this,message, Toast.LENGTH_SHORT).show();
     }
 
     public boolean isEmpty(String strInput){
